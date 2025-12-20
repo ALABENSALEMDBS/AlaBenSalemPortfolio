7403b0d613d70cdf6c63d42acae05255229cbccf
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-12-20 22:28:44 +0100

    competances DONE

---
+++ src/app/linding/competance/competance.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competance',
+  imports: [CommonModule],
+  templateUrl: './competance.component.html',
+  styleUrl: './competance.component.css'
+})
+export class CompetanceComponent {
+
+}
+
