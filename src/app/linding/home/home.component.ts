aedf952d06557ee08d36fe4c76634e3dc3328893
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-12-25 18:20:39 +0100

    add project component

--- src/app/linding/home/home.component.ts
+++ src/app/linding/home/home.component.ts
@@ -4,10 +4,11 @@ import { CompetanceComponent } from "../competance/competance.component";
 import { CertificationComponent } from "../certification/certification.component";
 import { HeropageComponent } from "../heropage/heropage.component";
 import { SkillsComponent } from "../skills/skills.component";
+import { ProjetComponent } from "../projet/projet.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AproposComponent, CertificationComponent, HeropageComponent, SkillsComponent],
+  imports: [AproposComponent, CertificationComponent, HeropageComponent, SkillsComponent, ProjetComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
