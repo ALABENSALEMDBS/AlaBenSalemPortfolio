68536234fed9c8952ab0a8fbd268c8fdc47fe80d
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-12-29 18:05:02 +0100

    fixed pdf resume show ans add contact component coming soon

---
+++ src/app/contact/contactforum/contactforum.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-contactforum',
+  imports: [RouterLink],
+  templateUrl: './contactforum.component.html',
+  styleUrl: './contactforum.component.css'
+})
+export class ContactforumComponent {
+
+}
