1740c937bea96c5d0ecaf3654283565dfd60f030
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2026-01-03 16:19:44 +0100

    ajouter un partie de parcours academic

--- src/app/linding/home/home.component.ts
+++ src/app/linding/home/home.component.ts
@@ -5,10 +5,11 @@ import { CertificationComponent } from "../certification/certification.component
 import { HeropageComponent } from "../heropage/heropage.component";
 import { SkillsComponent } from "../skills/skills.component";
 import { ProjetComponent } from "../projet/projet.component";
+import { StudiesComponent } from "../studies/studies.component";
 
 @Component({
   selector: 'app-home',
-  imports: [AproposComponent, CertificationComponent, HeropageComponent, SkillsComponent, ProjetComponent],
+  imports: [AproposComponent, CertificationComponent, HeropageComponent, SkillsComponent, ProjetComponent, StudiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
