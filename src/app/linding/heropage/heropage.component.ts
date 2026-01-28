859e5d8dca34054244fff2213b8b21c483d7274f
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2026-01-28 21:29:04 +0100

    ajouter contact  et la traduction de la page

--- src/app/linding/heropage/heropage.component.ts
+++ src/app/linding/heropage/heropage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-heropage',
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './heropage.component.html',
   styleUrl: './heropage.component.css'
 })
